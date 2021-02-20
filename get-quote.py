d53cb527af3a7ac291074d82a6bc1f358bc4d230
harrycoffey <79307580+harrycoffey@users.noreply.github.com>
2021-02-20 15:23:21 +0100

    Done intro

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,22 @@
+#function for reading
 def main():
-  print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[6])
+  print(*quotes,sep = "\n")
 
 if __name__== "__main__":
   main()
+
+# for writing new quote
+text_file.write('\nI made a balls of it');
+text_file.close()
+
+main()
+
+#n = text_file.write('Welcome to pythonexamples.org')
+#text_file.close()
+
+
